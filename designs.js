e00d1f7d2dfda79bd85073644247cbdd9b6a832d
shannon <shannonvan.edu@gmail.com>
2022-08-15 19:32:53 -0400

    design.js to be updated with the js code for the project.

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
 // Select color input
 // Select size input
+var color = document.querySelector("#colorPicker");
+var height = document.querySelector("#inputHeight");
+var width = document.querySelector("#inputWidth");
+var grid = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
+var sizeSelector = document.querySelector("#sizePicker");
+sizeSelector.addEventListener('submit', (event) => {
+    event.preventDefault();
+    makeGrid(height, width);
+});
 
-function makeGrid() {
-
-// Your code goes here!
-
+function refresh(){
+    let grid = document.querySelectorAll('tr');
+    grid.forEach((row) => {
+        row.remove();
+    });
 }
+
+function makeGrid(height, width) {
+ // Your code goes here!
+    refresh();
+    for (var i = 1; i <= height.value; i++){
+        var row = document.createElement('tr');
+        grid.append(row);
+        for (var j = 1; j <= width.value; j++) {
+            var col = document.createElement('td');
+            row.append(col);
+        }
+        grid.append(row);
+    }
+}
+
+grid.addEventListener('click', function colorCell(cell) {
+    cell.target.style.backgroundColor = color.value;
+ });
